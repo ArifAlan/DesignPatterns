921ab0519c8db7fe28321f60da63afb23ac31496
ArifAlan <91489739+ArifAlan@users.noreply.github.com>
2024-05-17 16:02:21 +0300

    unit-of-work

---
+++ UnitOfWorkDesignPattern/DesignPattern.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using DesignPattern.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DataAccessLayer.Concrete
+{
+	public class Context : DbContext
+	{
+		//public Context(DbContextOptions<Context> options) : base(options)
+		//{
+
+		//}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB; Initial Catalog=DesignPattern1;Integrated Security=True;");
+		}
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Process> Processes { get; set; }
+	}
+}
